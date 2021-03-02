ce634ecbee11a0a3ae28baf6c741b65bc91476e1
Generale Hablatitus <zenofrancesco99@gmail.com>
2021-03-02 01:34:05 +0100

    Update Curve di lissajous.py

--- Curve di lissajous.py
+++ Curve di lissajous.py
@@ -17,7 +17,7 @@ x = fx(t)
 y = fy(t)
 
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(1)
 plt.title("Curve di lissajous $\omega_x$= %.2f; $\omega_y$= %.2f" %(o,o1), fontsize=20)
 ax.set_xlim(np.min(x)-0.1,np.max(x)+0.1)
 ax.set_ylim(np.min(y)-0.1,np.max(y)+0.1)
@@ -38,7 +38,7 @@ anim = animation.FuncAnimation(fig, animate, frames=N, interval=1, blit=True, re
 #anim.save('Curve di lissajous.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 plt.show()
 ##
-plt.figure()
+plt.figure(2)
 plt.title("Curve di lissajous $\omega_x$= %.2f; $\omega_y$= %.2f" %(o,o1), fontsize=20)
 plt.xlabel('X(t)')
 plt.ylabel('Y(t)')
